fd8e7153fb12109be1a4c64939408660121affa5
Maltsev Vladimir <st054439@student.spbu.ru>
2017-04-07 12:43:58 +0300

    adds method delete()

--- TreeLib/BTree/BTree.kt
+++ TreeLib/BTree/BTree.kt
@@ -14,9 +14,7 @@ class BTree<Key : Comparable<Key>, Data> {
     fun isEmpty() = root.keys.size == 0
 
     fun search(key: Key): Data? {
-        var (parent, node) = searchNode(key = key)
-        if (node == null)
-            return null
+        var node = searchNode(key = key) ?: return null
 
         var i = 0
         while (i < node.keys.size && key > node.keys[i]) {
@@ -28,79 +26,26 @@ class BTree<Key : Comparable<Key>, Data> {
         return null
     }
 
-    private fun searchNode(parent: BNode<Key, Data> = root, node: BNode<Key, Data> = root,
-                           key: Key, isDelete: Boolean = false): Pair<BNode<Key, Data>?, BNode<Key, Data>?> {
+    private fun searchNode(node: BNode<Key, Data> = root,
+                           key: Key, isDelete: Boolean = false): BNode<Key, Data>? {
+        printTree()
         var i = 0
-        while (i < node.keys.size && key > node.keys[i])
+        while (i < node.keys.size && key > node.keys[i]) {
             i++
+            print(node.keys[i])
+            print(" ")
+        }
+        println(i)
 
         if (i < node.keys.size && key == node.keys[i])
-            return Pair(parent, node)
+            return node
 
         if (node.isLeaf())
-            return Pair(null, null)
+            return null
 
-        if (isDelete && node.children[i].keys.size < MIN_DIG) {
-            if (i + 1 < node.children.size && node.children[i + 1].keys.size >= MIN_DIG) run {
-                var key_parent = node.keys[i]
-                var data_parent = node.data[i]
 
-                node.keys.add(i, node.children[i + 1].keys.removeAt(0))
-                node.data.add(i, node.children[i + 1].data.removeAt(0))
 
-                node.children[i].keys.add(key_parent)
-                node.children[i].data.add(data_parent)
-
-                node.children[i].children.add(node.children[i + 1].children.removeAt(0))
-            } else if (i - 1 < node.children.size && node.children[i - 1].keys.size >= MIN_DIG) run {
-                var key_parent = node.keys[i - 1]
-                var data_parent = node.data[i - 1]
-
-                node.keys.add(i - 1, node.children[i - 1].keys.removeAt(node.children[i - 1].keys.size - 1))
-                node.data.add(i - 1, node.children[i - 1].data.removeAt(node.children[i - 1].data.size - 1))
-
-                node.children[i].keys.add(0, key_parent)
-                node.children[i].data.add(0, data_parent)
-
-                node.children[i].children.add(0, node.children[i - 1].children.removeAt(node.children[i - 1].children.size - 1))
-            } else {
-                if (i + 1 < node.children.size) {
-                    node.children[i].keys.add(node.keys[i])
-                    node.children[i].data.add(node.data[i])
-
-                    while (!node.children[i + 1].keys.isEmpty()) {
-                        node.children[i].keys.add(node.children[i + 1].keys.removeAt(0))
-                        node.children[i].data.add(node.children[i + 1].data.removeAt(0))
-                    }
-
-                    while (!node.children[i + 1].children.isEmpty()) {
-                        node.children[i].children.add(node.children[i + 1].children.removeAt(0))
-                    }
-
-                    node.keys.removeAt(i)
-                    node.data.removeAt(i)
-                    node.children.removeAt(i + 1)
-                } else {
-                    node.children[i].keys.add(0, node.keys[i - 1])
-                    node.children[i].data.add(0, node.data[i - 1])
-
-                    while (!node.children[i - 1].keys.isEmpty()) {
-                        node.children[i].keys.add(0, node.children[i - 1].keys.removeAt(node.children[i - 1].keys.size - 1))
-                        node.children[i].data.add(0, node.children[i - 1].data.removeAt(node.children[i - 1].data.size - 1))
-                    }
-
-                    while (!node.children[i - 1].children.isEmpty()) {
-                        node.children[i].children.add(0, node.children[i - 1].children.removeAt(node.children[i - 1].children.size - 1))
-                    }
-
-                    node.keys.removeAt(i - 1)
-                    node.data.removeAt(i - 1)
-                    node.children.removeAt(i - 1)
-                }
-            }
-        }
-
-        return searchNode(node, node.children[i], key)
+        return searchNode(node.children[i], key, isDelete)
     }
 
     private fun splitNode(parent: BNode<Key, Data>, i_median: Int, splitChild: BNode<Key, Data>) {
@@ -154,65 +99,116 @@ class BTree<Key : Comparable<Key>, Data> {
         }
     }
 
-    fun delete(key: Key) {
-        var (parent, maggie) = searchNode(key = key, isDelete = true) //maggie is a girl who was wanted to be die
-        if (maggie == null)
-            return
-
-        var ind = 0
-        while (ind < maggie.keys.size && key > maggie.keys[ind]) {
-            ind++
+    fun remove (key: Key, currNode : BNode<Key, Data> = root) {
+        var i = 0
+        while (i < currNode.keys.size && key > currNode.keys[i]) {
+            i++
         }
 
-        while (!maggie!!.isLeaf()) {
-            var node_victim = maggie
-            if (node_victim!!.children[ind].keys.size >= MIN_DIG) {
-                node_victim = node_victim.children[ind]
-                while (!node_victim!!.isLeaf()) {
-                    node_victim = node_victim.children[node_victim.children.size - 1]
-                }
-                maggie.keys[ind] = node_victim.keys[node_victim.keys.size - 1]
-                maggie.data[ind] = node_victim.data[node_victim.data.size - 1]
-
-                ind = node_victim.keys.size - 1
-                maggie = node_victim
+        if (i < currNode.keys.size && key == currNode.keys[i])
+        {
+            if (currNode.isLeaf()) {
+                currNode.keys.removeAt(i)
+                currNode.data.removeAt(i)
             } else {
-                if (node_victim.children[ind + 1].keys.size >= MIN_DIG) {
-                    node_victim = node_victim!!.children[ind + 1]
+                var node_victim = currNode
+
+                if (node_victim.children[i].keys.size >= MIN_DIG) {
+                    node_victim = node_victim.children[i]
                     while (!node_victim!!.isLeaf()) {
-                        node_victim = node_victim.children[0]
+                        node_victim = node_victim.children[node_victim.children.size - 1]
                     }
-                    maggie.keys[ind] = node_victim.keys[0]
-                    maggie.data[ind] = node_victim.data[0]
 
-                    ind = 0
-                    maggie = node_victim
+                    var new_key = node_victim.keys[node_victim.keys.size - 1]
+                    var new_data = node_victim.data[node_victim.data.size - 1]
+
+                    remove(new_key, currNode)
+
+                    currNode.keys[i] = new_key
+                    currNode.data[i] = new_data
+
                 } else {
-                    maggie.children[ind].keys.add(maggie.keys[ind])
-                    maggie.children[ind].data.add(maggie.data[ind])
+                    if (node_victim.children[i + 1].keys.size >= MIN_DIG) {
+                        node_victim = node_victim.children[i + 1]
+                        while (!node_victim!!.isLeaf()) {
+                            node_victim = node_victim.children[0]
+                        }
+                        var new_key = node_victim.keys[0]
+                        var new_data = node_victim.data[0]
 
-                    while (!maggie.children[ind + 1].keys.isEmpty()) {
-                        maggie.children[ind].keys.add(maggie.children[ind + 1].keys.removeAt(0))
-                        maggie.children[ind].data.add(maggie.children[ind + 1].data.removeAt(0))
+                        remove(new_key, currNode)
+
+                        currNode.keys[i] = new_key
+                        currNode.data[i] = new_data
+                    } else {
+                        currNode.children[i].keys.add(currNode.keys[i])
+                        currNode.children[i].data.add(currNode.data[i])
+
+                        while (!currNode.children[i + 1].keys.isEmpty()) {
+                            currNode.children[i].keys.add(currNode.children[i + 1].keys.removeAt(0))
+                            currNode.children[i].data.add(currNode.children[i + 1].data.removeAt(0))
+                        }
+
+                        while (!currNode.children[i + 1].children.isEmpty()) {
+                            currNode.children[i].children.add(currNode.children[i + 1].children.removeAt(0))
+                        }
+
+                        currNode.keys.removeAt(i)
+                        currNode.data.removeAt(i)
+                        currNode.children.removeAt(i + 1)
+                        remove(key, currNode.children[i])
                     }
-
-                    while (!maggie.children[ind + 1].children.isEmpty()) {
-                        maggie.children[ind].children.add(maggie.children[ind + 1].children.removeAt(0))
-                    }
-
-                    maggie.keys.removeAt(ind)
-                    maggie.data.removeAt(ind)
-                    maggie.children.removeAt(ind + 1)
-                    maggie = maggie.children[ind]
-                    ind = maggie.keys.indexOf(key)
                 }
             }
+        } else {
+            if (currNode.isLeaf())
+                return
+
+            if (currNode.children[i].keys.size < BTree.MIN_DIG) {
+                if (currNode.children[i + 1].keys.size >= BTree.MIN_DIG) {
+                    var key_parent = currNode.keys[i]
+                    var data_parent = currNode.data[i]
+
+                    currNode.keys.add(i, currNode.children[i + 1].keys.removeAt(0))
+                    currNode.data.add(i, currNode.children[i + 1].data.removeAt(0))
+
+                    currNode.children[i].keys.add(key_parent)
+                    currNode.children[i].data.add(data_parent)
+
+                    currNode.children[i].children.add(currNode.children[i + 1].children.removeAt(0))
+                } else
+                    if (currNode.children[i - 1].keys.size >= BTree.MIN_DIG) {
+                        var key_parent = currNode.keys[i - 1]
+                        var data_parent = currNode.data[i - 1]
+
+                        currNode.keys.add(i - 1, currNode.children[i - 1].keys.removeAt(currNode.children[i - 1].keys.size - 1))
+                        currNode.data.add(i - 1, currNode.children[i - 1].data.removeAt(currNode.children[i - 1].data.size - 1))
+
+                        currNode.children[i].keys.add(0, key_parent)
+                        currNode.children[i].data.add(0, data_parent)
+
+                        currNode.children[i].children.add(0, currNode.children[i - 1].children.removeAt(currNode.children[i - 1].children.size - 1))
+                    } else {
+                        currNode.children[i].keys.add(currNode.keys.removeAt(i))
+                        currNode.children[i].data.add(currNode.data.removeAt(i))
+
+                        while (!currNode.children[i + 1].keys.isEmpty()) {
+                            currNode.children[i].keys.add(currNode.children[i + 1].keys.removeAt(0))
+                            currNode.children[i].data.add(currNode.children[i + 1].data.removeAt(0))
+                        }
+
+                        while (!currNode.children[i + 1].children.isEmpty()) {
+                            currNode.children[i].children.add(currNode.children[i + 1].children.removeAt(0))
+                        }
+
+                        currNode.children.removeAt(i + 1)
+                    }
+
+            }
+
         }
 
-        maggie.data.removeAt(ind)
-        maggie.keys.removeAt(ind)
     }
-
 
     fun printTree() {
         var list: Queue<BNode<Key, Data>> = LinkedList()
