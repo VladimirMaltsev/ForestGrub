1e9b5a056b8d838af1fb4399d0777d868cf8993a
Maltsev Vladimir <st054439@student.spbu.ru>
2017-04-11 16:55:03 +0300

    override method equals for RBNode

--- TreeLib/RBTree/RBNode.kt
+++ TreeLib/RBTree/RBNode.kt
@@ -10,4 +10,22 @@ class RBNode<Key, Data>(var key: Key, var data: Data){
     var leftChild: RBNode<Key, Data>? = null
     var rightChild: RBNode<Key, Data>? = null
 
+    override operator fun equals(other: Any?): Boolean {
+        if (other is RBNode<*, *>?)
+        {
+            if (other == null)
+                return false
+
+            if (this.key == other.key &&
+                    this.data == other.data &&
+                    this.isRed == other.isRed /*&&
+                    this.parent == other.parent &&
+                    this.rightChild == other.rightChild&&
+                    this.leftChild == other.leftChild*/)
+                return true
+
+            return false
+        }
+        return false
+    }
 }
